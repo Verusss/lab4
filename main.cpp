6066629c7a56dd0d771519554bd973347f47843a
Verusss <226327@student.pwr.edu.pl>
2016-04-19 17:38:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+using namespace std;
+
+int main (int argc, char *argv[])
 {
-	printf("Hello, World!\n");
+	int n=atoi(argv[1]);
+	printf ("Dla n=%d, liczbą złożoną z samych zer i jedynek jest zero. Aby ją otrzymać, należy pomnożyć n przez zero.\n", n);
+	return 0;
 }
+//Pozdrawiam. Normalna wersja programu też będzie. 
